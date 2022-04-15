de590fa7c4a6bdffc59b649798e11130ed3eab0f
Promisekel <promisebansah12@gmail.com>
2022-04-15 15:15:51 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "!main.h"
+/**
+ * _isupper - evalue if letter is uppercase .
+ * @c: print int
+ * Reurn: Always 0.
+ */
+ 
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+}
+Return (1);
+}
+
+else
+{
+	return (0)
+}
+
+}
